f77837d68d42e20602a8e868308d819fca8758b7
whitedragon115 <stdn1255010@goo.tyai.tyc.edu.tw>
2025-11-12 14:17:52 +0800

    update text color

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ import ShinyText from '@/components/ShinyText';
 import { useEffect, useState, useRef } from 'react';
 
 export default function Home() {
-  
+
   const moveDistance = 20;
 
   const [position, setPosition] = useState({ x: 0, y: 0 })
@@ -36,13 +36,13 @@ export default function Home() {
           style={{
             transform: `translate(${position.x}px, ${position.y}px)`,
           }}
-          >
+        >
           <LetterGlitch
             glitchColors={["#1a2a6c", "#4a5fc1", "#7b8cde", "#a0aeff"]}
             outerVignette={false}
             centerVignette={true}
             characters='-----------------------------+01'
-            />
+          />
         </div>
         <div
           className='w-[50vw] h-[10vh] fixed -top-1 p-3 backdrop-blur-sm bg-white/10'
@@ -50,17 +50,20 @@ export default function Home() {
             borderRadius: '0 0 50px 50px',
             boxShadow: 'rgba(255, 255, 255, 0.2) 0 0 50px',
           }}
-          >
+        >
           <div className='w-full h-full p-2 rounded-full flex flex-row items-center justify-between backdrop-blur-sm'>
-            <div className='ml-5 text-blue-200 text-3xl emfont-FusionPixelFont10pxMono'>
+            <div className='ml-5 text-gray-300 text-3xl emfont-FusionPixelFont10pxMono'>
               <span className=''>Dragon Tools</span>
-              <span className='ml-2 text-sm text-gray-400'>v2.0.1</span>
+              <ShinyText
+                text="V 0.0.1"
+                className='ml-2 text-sm text-gray-400'
+              />
             </div>
             <div className='h-full mr-[0.4] aspect-square bg-gray-500 rounded-full flex items-center justify-center transition-all duration-300 ease-in-out hover:scale-105'>
               <ClosingLink
                 href="/tools"
                 className="rounded-full cursor-pointer group flex items-center justify-center transition-all duration-300 ease-in-out hover:ring-2 hover:ring-white/40 active:scale-95 hover:scale-110"
-                >
+              >
                 <FaTools className='text-white text-3xl' />
               </ClosingLink>
             </div>
@@ -69,12 +72,12 @@ export default function Home() {
 
         <div className='text-center relative z-10'
           style={{
-            transform: `translate(${-(position.x/2)}px, 0px)`,
+            transform: `translate(${-(position.x / 2)}px, 0px)`,
           }}
         >
           {/* @ts-ignore */}
           <TextType
-            className='text-7xl emfont-FusionPixelFont10pxMono text-blue-100'
+            className='text-7xl emfont-FusionPixelFont10pxMono text-gray-200'
             text={["Dragon tools", "Made by Dragon"]}
             pauseDuration={3000}
             showCursor={true}
